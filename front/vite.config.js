0a9dd627c3cff6881c607d11f6c1992dc4736a71
LizZzDev <lizethcruz181112@gmail.com>
2026-05-18 10:02:47 -0600

    a2

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // <-- ESTO ES OBLIGATORIO PARA DOCKER
+    port: 5173,
+  }
 })
